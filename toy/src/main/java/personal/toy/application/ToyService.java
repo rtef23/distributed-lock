f50c33503f2364270a35493772bbfdf50f564d0c
jdra <rtef23@gmail.com>
2021-07-05 07:02:40 +0900

    1. flush 명시적으로 하도록 수정

--- toy/src/main/java/personal/toy/application/ToyService.java
+++ toy/src/main/java/personal/toy/application/ToyService.java
@@ -1,8 +1,9 @@
 package personal.toy.application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import personal.toy.domain.Test;
 import personal.toy.infrastructure.TestRepository;
@@ -14,10 +15,16 @@ public class ToyService {
 
   private final TestRepository testRepository;
 
-  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  //  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  @Transactional
   public void test(long id, TestDto testDto) {
     Test test = testRepository.findById(id).orElseThrow(IllegalStateException::new);
 
     test.addValue(testDto.getValue());
+
+    entityManager.flush();
   }
 }
